55d707378cfa797c6ebb871c4c0d1cdbb6933110
Vadim <115623536+vadimvalov@users.noreply.github.com>
2026-03-28 04:40:20 +0500

    fix: 1d grid

--- seminar-1-2-3/src/app/src/main/java/org/simulation/domain/Grid2D.java
+++ seminar-1-2-3/src/app/src/main/java/org/simulation/domain/Grid2D.java
@@ -32,7 +32,34 @@ public class Grid2D implements SimulationDomain<SimulationDomain.Dim2D> {
 
     public int getIndex(int i, int j) { return j * nx + i; }
     
-    public double computeLaplacian(double[] values, int i, int j) {
-        return 0.0; // Simulated
+    // Demonstrate 2D logic: 5-point stencil
+    public double computeLaplacian(Field<Double> field, int i, int j) {
+        double u_ij = field.getValue(i, j);
+        return (field.getValue(i+1, j) + field.getValue(i-1, j) + 
+                field.getValue(i, j+1) + field.getValue(i, j-1) - 4.0 * u_ij) / (dx * dx);
+    }
+}
+
+/**
+ * Demonstrate 1D Grid with specialized 3-point stencil logic.
+ */
+class Grid1D implements SimulationDomain<SimulationDomain.Dim1D> {
+    private final int nx;
+    private final double dx;
+
+    public Grid1D(int nx, double lx) {
+        this.nx = nx;
+        this.dx = lx / (nx - 1);
+    }
+
+    @Override public int getNx() { return nx; }
+    @Override public int getNy() { return 1; }
+    @Override public double getDx() { return dx; }
+    @Override public double getDy() { return 0.0; }
+    @Override public void applyBoundaryConditions(Field<Double> field) {}
+
+    // Demonstrate 1D logic: 3-point stencil
+    public double computeLaplacian(Field<Double> field, int i) {
+        return (field.getValue(i+1, 0) + field.getValue(i-1, 0) - 2.0 * field.getValue(i, 0)) / (dx * dx);
     }
 }
